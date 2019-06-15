6409b92aaa69ac7e52b0aa5a47495705271e1c5e
ReyesRichard <ricardo08@live.com.mx>
2019-06-15 14:05:19 -0500

    Add models

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,9 @@
+export class PostModel{
+    constructor(
+        public id:number,
+        public title:string,
+        public date:string,
+        public description:string,
+        public author:number,
+    ){}
+}
